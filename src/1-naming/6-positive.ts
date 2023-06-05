642431b770362f1507883d5890d7d223c3e92646
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-05 15:34:29 +0200

    fix: comments

--- src/1-naming/6-positive.ts
+++ src/1-naming/6-positive.ts
@@ -1,11 +1,11 @@
 // ❌
-const isNotEmpty = Math.random() > 0.5;
-// 🤢 express conditions in positive
-if (isNotEmpty === false) {
-  console.log("do nothing");
-} else {
-  console.log("do something");
-}
+// const isNotEmpty = Math.random() > 0.5;
+// // 🤢 express conditions in positive
+// if (isNotEmpty === false) {
+//   console.log("do nothing");
+// } else {
+//   console.log("do something");
+// }
 
 const isNotEmpty = Math.random() > 0.5;
 const hasData = !isNotEmpty;
